aae961c561ac926381797b68a9434db899023ec1
Mihály Tringel <mtringel@trmsoftwaredesignkft.onmicrosoft.com>
2024-05-15 18:54:54 +0200

    Small refactoring, adding 'readonly'

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -12,28 +12,28 @@ export type ValueOrArray<Value> = Value | Value[];
 export type Guid = string;
 
 export interface StringCompareOptions {
-	ignoreCase?: boolean;
-	trim?: boolean;
+	readonly ignoreCase?: boolean;
+	readonly trim?: boolean;
 }
 
 export type DatePart = "year" | "month" | "day" | "hour" | "minute" | "second" | "millisecond";
 
 export interface DateTimeFormatOptions {
-	seconds?: boolean;
-	milliseconds?: boolean;
+	readonly seconds?: boolean;
+	readonly milliseconds?: boolean;
 
 	// shift time zone offset before converting
 	// - specifying 'true' results the same behavior how date.toString() works
 	// - specifying 'false' results the same behavior how date.toLocalDateString() works
-	utc?: boolean;
+	readonly utc?: boolean;
 }
 
 export interface NumberFormatOptions {
-	minIntegerDigits?: number; // specify 2 and 3 will become "03", maximum value: 100
-	radix?: number; // default is 10
-	minDecimalDigits?: number; // zeroes will be added to reach this length
-	maxDecimalDigits?: number; // fractional part will be cut over this part (note: there is no rounding!)
-	thousandSeparators?: boolean; // default is 'true'
+	readonly minIntegerDigits?: number; // specify 2 and 3 will become "03", maximum value: 100
+	readonly radix?: number; // default is 10
+	readonly minDecimalDigits?: number; // zeroes will be added to reach this length
+	readonly maxDecimalDigits?: number; // fractional part will be cut over this part (note: there is no rounding!)
+	readonly thousandSeparators?: boolean; // default is 'true'
 }
 
 // -1 if first argument is less, 1 if first argument is greater, 0 if compared arguments are equal
@@ -47,10 +47,10 @@ export enum ContentTypeCategory {
 }
 
 export interface ContentType {
-	name: string;
-	allowedContentTypes: string[];
-	allowedExtensions: string[];
-	categories: ContentTypeCategory[];
+	readonly name: string;
+	readonly allowedContentTypes: string[];
+	readonly allowedExtensions: string[];
+	readonly categories: ContentTypeCategory[];
 }
 
 export type StateSetter<State> = (
@@ -68,11 +68,11 @@ export interface StorybookComponent<P = never> {
 }
 
 export interface ObjectChildMemberAccessOptions {
-	pathSeparator?: string; // used only if memberNamesPath is string, not a string array; default is '.'
+	readonly pathSeparator?: string; // used only if memberNamesPath is string, not a string array; default is '.'
 
 	// if specified and memberNamesPath starts with "/" then the evaluation will start at the root object, not the parameter object
-	rootObj?: unknown;
+	readonly rootObj?: unknown;
 
 	// if specified and memberNamesPath starts with "@name" then the evaluation will start at the named object found here, not the parameter object
-	namedObjs?: { [name: string]: unknown };
+	readonly namedObjs?: { [name: string]: unknown };
 }
